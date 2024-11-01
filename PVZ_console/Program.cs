554fa029ce9c3162d0eb701c73dcf26e3922d4f3
KubeSlide <110945154+Kube-slide@users.noreply.github.com>
2024-11-01 10:12:13 -0400

    Added mouse input
    - Added mouse detection
    - Added button detection
    - Added check to see if mouse is in window

--- PVZ_console/Program.cs
+++ PVZ_console/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Security.Permissions;
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
 namespace PVZ_console
 {
     internal class Program
@@ -10,6 +12,32 @@ namespace PVZ_console
         static bool preload = false;
         static string[] gameStates = { "Menu", "In-game", "Paused"};
         static string curState;
+
+        // We need to use unmanaged code
+        [DllImport("user32.dll")]
+
+        // GetCursorPos() makes everything possible
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+        //More unmanaged code
+        [DllImport("user32.dll")]
+        //Call window function
+        static extern IntPtr GetForegroundWindow();
+
+        //MORE unmanaged code
+        [DllImport("user32.dll")]
+        //Get window sizes
+        static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        //Create a struct to store the coord system for out window
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
 
         //Run every function every frame
         static void Main(string[] args)
@@ -25,7 +53,10 @@ namespace PVZ_console
             {
                 Draw();
             }
-            Thread.Sleep(1000);
+            //Sleep for 1/4 of a second --> 4 fps? doesnt flash eyes too bad
+            Thread.Sleep(250);
+
+            //Clear console (failsafe) and loop :|
             Console.Clear();
             Main(null);
 
@@ -72,7 +103,10 @@ namespace PVZ_console
         //Draw game screen
         static void Draw()
         {
+            //Clear previous frame
             Console.Clear();
+
+            //Check our current state --> outdated method imo but works for now ig
             switch (curState)
             {
                 case "Menu":
@@ -85,13 +119,75 @@ namespace PVZ_console
                     Console.WriteLine("Game paused");
                     break;
             }
+            //End of frame--> accept player inputs
+            PlayerInputs();
         }
 
+        //Draw main menu
         static void DMM()
         {
-            Console.WriteLine("                                   \r\n    =+=     =-======-=             \r\n -=++*#*+-==:::::----===           \r\n +*###*-+-..::---*#=-=*#+= -====-  \r\n=+*#*+:+:..:----=-#@*=#%==--=**+==-\r\n =*+  ==..:------+%%=====-=%@@@*++-\r\n  --  ==:::-----========-=#@@@@#++-\r\n      :+-------===========%@@@%++==\r\n        ================+=+##*+==- \r\n         =++=========+++:======-   \r\n          :*###**+++==             \r\n           ==++:                   \r\n           =-=+                    \r\n       -+++-+-+-===                \r\n      =******==****+==:            \r\n      ===+++**=+*++++==+:          \r\n     +++++++++**+++++++++:         \r\n     -++++++++- ==++++=--          \r\n         ::.                       u");
-            Console.WriteLine("\n\n\n\n  ____  _             _                                        _     _           \r\n |  _ \\| | __ _ _ __ | |_ ___  __   _____   _______  _ __ ___ | |__ (_) ___  ___ \r\n | |_) | |/ _` | '_ \\| __/ __| \\ \\ / / __| |_  / _ \\| '_ ` _ \\| '_ \\| |/ _ \\/ __|\r\n |  __/| | (_| | | | | |_\\__ \\  \\ V /\\__ \\  / / (_) | | | | | | |_) | |  __/\\__ \\\r\n |_|   |_|\\__,_|_| |_|\\__|___/   \\_/ |___/ /___\\___/|_| |_| |_|_.__/|_|\\___||___/\r\n                     / ___|___  _ __  ___  ___ | | ___                           \r\n                    | |   / _ \\| '_ \\/ __|/ _ \\| |/ _ \\                          \r\n                    | |__| (_) | | | \\__ \\ (_) | |  __/                          \r\n                     \\____\\___/|_| |_|___/\\___/|_|\\___|                          \r\n                                                                                ");
+            Console.WriteLine("Main menu");
+            Console.WriteLine("1.Options\n2.Play game\n3.Exit\n4.Credits");
         }
+
+        //Function to get mousePosition. Returns (int, int) --> relative mousePOS to console
+        static (int, int) GetMouseInput()
+        {
+            // New point that will be updated by the function with the current coordinates
+            Point defPnt = new Point();
+
+            // Call the function and pass the Point, defPnt
+            GetCursorPos(ref defPnt);
+
+            //Get our consoleWindow position for reference
+            IntPtr consoleHandle = GetForegroundWindow();
+
+            //Get our window size with coords
+            GetWindowRect(consoleHandle, out RECT consoleRect);
+
+            //Return mouse position for future ref
+            return (defPnt.X - consoleRect.Left, defPnt.Y - consoleRect.Top);
+        }
+
+        //Function to check if mouse is within window --> returns bool
+        static bool WithinWindow()
+        {
+            //Get our consoleWindow position for reference
+            IntPtr consoleHandle = GetForegroundWindow();
+
+            //Get our window size with coords
+            GetWindowRect(consoleHandle, out RECT consoleRect);
+
+            // New point that will be updated by the function with the current coordinates
+            Point defPnt = new Point();
+
+            // Call the function and pass the Point, defPnt
+            GetCursorPos(ref defPnt);
+
+            bool xInRange = defPnt.X >= consoleRect.Left && defPnt.X <= consoleRect.Right;
+            bool yInRange = defPnt.Y >= consoleRect.Top && defPnt.Y <= consoleRect.Bottom;
+            return xInRange && yInRange;
+        }
+
+        //Getting player mouse inputs and accept command input --> will be used to detect 'Grab plant' / 'Place plant' / 'Collect sun'
+        static void PlayerInputs()
+        {
+            //Start by getting mousePOS every 'frame'
+            (int, int) MousePos = GetMouseInput();
+            bool isInWindow = WithinWindow();
+
+            //Check for input WITHOUT blocking script. W/o console.KeyAvailable, the rest of the code would hang :\
+            if(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Spacebar && isInWindow)
+            {
+                Console.WriteLine($"Interact with user at location {MousePos}");
+            }
+            else if (!isInWindow)
+            {
+                Console.WriteLine("Outside game area! Return back to keep playing!");
+            }
+
+        }
+
     }
 
     //Creating a plant + executing plant logic
